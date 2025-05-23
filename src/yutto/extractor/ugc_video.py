9e64a3efcb8e27d071815b214084f4342e88798a
XnneHang <XnneHang@gmail.com>
2025-05-23 11:45:40 +0800

    :recycle: refactor: add episode identifier for single_ugc_video (#6)
    * 添加 page 作为不同 p 的 keywords
    
    * fmt

--- src/yutto/extractor/ugc_video.py
+++ src/yutto/extractor/ugc_video.py
@@ -84,7 +84,9 @@ class UgcVideoExtractor(SingleExtractor):
         try:
             ugc_video_list = await get_ugc_video_list(ctx, client, self.avid)
             self.avid = ugc_video_list["avid"]  # 当视频撞车时，使用新的 avid 替代原有 avid，见 #96
+            ugc_video_list["title"] = ugc_video_list["title"] + f"_p{self.page}"
             Logger.custom(ugc_video_list["title"], Badge("投稿视频", fore="black", back="cyan"))
+
             return CoroutineWrapper(
                 extract_ugc_video_data(
                     ctx,
